b8df0f0f2867e68ddcb5a2be363ff343997b66b8
Evandro Madeira <evandromfmadeira@gmail.com>
2018-09-24 16:07:55 -0300

    SignUp
    Criado tela de signup

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,18 +1,23 @@
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';         
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [
     ReactiveFormsModule,
-    CommonModule,
     VMessageModule,
-    RouterModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
