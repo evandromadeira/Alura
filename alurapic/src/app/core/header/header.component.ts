62c85de28143bc35ee1891553d389b1376d9a13d
Evandro Madeira <evandromfmadeira@gmail.com>
2018-09-24 14:13:36 -0300

    Header
    Criação do header com impressão do usuário usando o JWT Decode.
    Utilizado "BehaviorSubject" para manter o usuário quando a página é atualizada.
    Tratado para só mostrar o usuário quando houver um, e "Please, login!" quando não houver.

---
+++ alurapic/src/app/core/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+
+@Component({
+  selector: 'ap-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+  user$: Observable<User>;
+
+  constructor(private userService: UserService) {
+    this.user$ = userService.getUser();
+  }
+}
