3ce6caec9d97aeddce02cb77675234271b8c9278
Evandro Madeira <evandromfmadeira@gmail.com>
2018-09-26 08:36:42 -0300

    Photo Details 2
    Correção e ajustes do componente de Photo Details.
    Criado "photo-details.css" para implementação da classe ".break-word" utilizado no templateUrl.

---
+++ alurapic/src/app/photos/photo-details/photo-details.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PhotoDetailsComponent } from './photo-details.component';
+import { PhotoModule } from '../photo/photo.module';
+
+@NgModule({
+  declarations: [PhotoDetailsComponent],
+  exports: [PhotoDetailsComponent],
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
+})
+export class PhotoDetailsModule { }
--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { PhotoDetailsModule } from './photo-detail/photo-details.module';
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoModule } from './photo/photo.module';
